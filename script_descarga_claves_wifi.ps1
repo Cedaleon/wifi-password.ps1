8027de7cfa457fbf64d153733572c306d556eaf4
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 18:42:53 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -1,10 +1,3 @@
-# Verifica si el equipo está conectado a una red Wi-Fi
-$connected = netsh wlan show interfaces | Select-String "Conectado"
-if ($connected -eq $null) {
-    Write-Host "El equipo no está conectado a una red Wi-Fi." -ForegroundColor Red
-    return
-}
-
 # Verifica la versión de PowerShell y que se está ejecutando en un equipo con Windows
 if ($PSVersionTable.PSVersion.Major -lt 3) {
     Write-Host "Este script requiere PowerShell 3.0 o posterior." -ForegroundColor Red
@@ -16,32 +9,27 @@ if (-not (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
     return
 }
 
-# Carpeta donde se encuentra el script
-$scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Path
+# Cambiar el nombre del archivo de salida aquí:
+$nombre_archivo = "pass_wifi.txt"
 
-# Obtener el nombre del perfil de la red Wi-Fi conectada
-$perfil = (netsh wlan show interfaces | Select-String "Perfil de todos los usuarios" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }).ToString()
-
-# Obtener la contraseña del perfil de la red Wi-Fi conectada
-$contrasena = (netsh wlan show profile name=$perfil key=clear | Select-String "Contenido de la clave" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }).ToString()
-
-# Solicitar el nombre de la red al usuario
-$nombre_red = Read-Host "Ingrese el nombre de la red Wi-Fi"
+# Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
+$contrasenas_wifi = netsh wlan show profile | Select-String "\bTodos los usuarios\b|\bUsuario actual\b" | %{(netsh wlan show profile name=$_.matches[0].value key=clear)}
 
 # Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
-$ruta_archivo = Join-Path -Path $scriptFolder -ChildPath "pass_wifi.txt"
-if (Test-Path -Path $ruta_archivo) {
-    $confirm = Read-Host "El archivo pass_wifi.txt ya existe en la carpeta $scriptFolder. ¿Desea sobrescribirlo? (S/N)"
+if (Test-Path -Path $nombre_archivo) {
+    $confirm = Read-Host "El archivo $nombre_archivo ya existe. ¿Desea sobrescribirlo? (S/N)"
     if ($confirm -ne 'S') {
         Write-Host "Operación cancelada." -ForegroundColor Yellow
         return
+    } else {
+        Remove-Item -Path $nombre_archivo -ErrorAction SilentlyContinue
     }
 }
 
-# Escribir el nombre de la red y la contraseña en el archivo de salida
+# Escribir las contraseñas en el archivo de salida
 try {
-    Set-Content -Path $ruta_archivo -Value "$nombre_red:`n$contrasena"
-    Write-Host "Contraseña guardada exitosamente en $ruta_archivo"
+    $contrasenas_wifi | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
+    Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
 } catch {
     Write-Host "Error al escribir en el archivo: $_.Exception.Message"
 }
