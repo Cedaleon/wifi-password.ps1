ad16091a80c8a41bfe4250149dd1accfcd47d15d
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 17:47:30 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -9,15 +9,21 @@ if (-not (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
     return
 }
 
+# Carpeta donde se encuentra el script
+$scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Path
+
 # Cambiar el nombre del archivo de salida aquí:
 $nombre_archivo = "pass_wifi.txt"
+
+# Ruta completa del archivo de salida
+$ruta_archivo = Join-Path -Path $scriptFolder -ChildPath $nombre_archivo
 
 # Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
 $contrasenas_wifi = netsh wlan show profile | Select-String "\bTodos los usuarios\b|\bUsuario actual\b" | %{(netsh wlan show profile name=$_.matches[0].value key=clear)}
 
 # Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
-if (Test-Path -Path $nombre_archivo) {
-    $confirm = Read-Host "El archivo $nombre_archivo ya existe. ¿Desea sobrescribirlo? (S/N)"
+if (Test-Path -Path $ruta_archivo) {
+    $confirm = Read-Host "El archivo $nombre_archivo ya existe en la carpeta $scriptFolder. ¿Desea sobrescribirlo? (S/N)"
     if ($confirm -ne 'S') {
         Write-Host "Operación cancelada." -ForegroundColor Yellow
         return
@@ -26,8 +32,8 @@ if (Test-Path -Path $nombre_archivo) {
 
 # Escribir las contraseñas en el archivo de salida
 try {
-    Set-Content -Path $nombre_archivo -Value $contrasenas_wifi
-    Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
+    Set-Content -Path $ruta_archivo -Value $contrasenas_wifi
+    Write-Host "Contraseñas guardadas exitosamente en $ruta_archivo"
 } catch {
     Write-Host "Error al escribir en el archivo: $_.Exception.Message"
 }
