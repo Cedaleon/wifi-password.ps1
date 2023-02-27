b9b9d658ebb5cccdce1e1e8202f1b832f81781b1
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 14:43:40 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -2,3 +2,4 @@ $url = "https://raw.githubusercontent.com/Cedaleon/wifi-password.ps1/main/wifi-p
 $archivo = "claves_wifi.txt"
 Invoke-WebRequest -Uri $url -OutFile wifi-password.ps1
 .\wifi-password.ps1 | Out-File -FilePath $archivo
+
