acf342545a0f047bb8f744d3f63db8e6876db648
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 19:33:30 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -18,6 +18,7 @@ $contrasenas_wifi = foreach ($red in $redes_wifi) {
     $nombre_red = $red -replace ".*:\s*(.*)", '$1'
     $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear) -replace "(?ms).*Clave de seguridad.*:\s*(.*)\s*\n.*", '$1'
     if ($contrasena_red) {
+    $nombre_red = $redes_wifi[$i].SSID.ToString()
         "$nombre_red: $contrasena_red"
     }
 }
