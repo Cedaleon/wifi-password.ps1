4992445fbaaed70af856dd9e7205383dca5beed7
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 20:42:36 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -16,17 +16,14 @@ $nombre_archivo = "pass_wifi.txt"
 $redes_wifi = netsh wlan show profile
 $contrasenas_wifi = foreach ($red in $redes_wifi) {
     $nombre_red = $red -replace ".*:\s*(.*)", '$1'
-    $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear) -replace "(?ms).*Clave de seguridad.*:\s*(.*)\s*\n.*", '$1'
+    $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear) -replace "(?ms).*Clave de seguridad.*:\s*(.*?)\s*\n.*", '$1'
     if ($contrasena_red) {
-        [PSCustomObject]@{
-            "Nombre de red" = $nombre_red
-            "Contraseña" = $contrasena_red
-        }
+        "${nombre_red}: ${contrasena_red}"
     }
 }
 
 # Ordenar las contraseñas alfabéticamente por nombre de red
-$contrasenas_wifi = $contrasenas_wifi | Sort-Object "Nombre de red"
+$contrasenas_wifi = $contrasenas_wifi | Sort-Object
 
 # Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
 if (Test-Path -Path $nombre_archivo) {
@@ -41,7 +38,7 @@ if (Test-Path -Path $nombre_archivo) {
 
 # Escribir las contraseñas ordenadas en el archivo de salida
 try {
-    $contrasenas_wifi | Format-Table -AutoSize | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
+    $contrasenas_wifi | Format-Table -Property @{Label="Red";Expression={$_.Split(":")[0]}},@{Label="Contraseña";Expression={$_.Split(":")[1]}} | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
     Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
 } catch {
     Write-Host "Error al escribir en el archivo: $($_.Exception.Message)" -ForegroundColor Red
