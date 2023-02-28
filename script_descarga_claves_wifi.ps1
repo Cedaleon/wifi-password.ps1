4e3126502bdf10f1a426f6f34d8a0d2d8d260575
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 21:18:19 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -1,4 +1,4 @@
-# Verifica la versión de PowerShell y que se está ejecutando en un equipo con Windows
+
 if ($PSVersionTable.PSVersion.Major -lt 3) {
     Write-Host "Este script requiere PowerShell 3.0 o posterior." -ForegroundColor Red
     return
@@ -9,23 +9,19 @@ if (-not (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
     return
 }
 
-# Cambiar el nombre del archivo de salida aquí:
 $nombre_archivo = "pass_wifi.txt"
 
-# Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
 $redes_wifi = netsh wlan show profile
 $contrasenas_wifi = foreach ($red in $redes_wifi) {
     $nombre_red = $red -replace ".*:\s*(.*)", '$1'
     $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear) -replace "(?ms).*Clave de seguridad.*:\s*(.*)\s*\n.*", '$1'
     if ($contrasena_red) {
-        "${nombre_red} - ${contrasena_red}"
+        "${nombre_red}: ${contrasena_red}"
     }
 }
 
-# Ordenar las contraseñas alfabéticamente por nombre de red
-$contrasenas_wifi = $contrasenas_wifi | Sort-Object
+$contrasenas_wifi = $contrasenas_wifi | Sort-Object {$_.Split(":")[0]}
 
-# Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
 if (Test-Path -Path $nombre_archivo) {
     $confirm = Read-Host "El archivo $nombre_archivo ya existe. ¿Desea sobrescribirlo? (S/N)"
     if ($confirm -ne 'S') {
@@ -36,7 +32,6 @@ if (Test-Path -Path $nombre_archivo) {
     }
 }
 
-# Escribir las contraseñas ordenadas en el archivo de salida
 try {
     $contrasenas_wifi | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
     Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
@@ -44,7 +39,6 @@ try {
     Write-Host "Error al escribir en el archivo: $($_.Exception.Message)" -ForegroundColor Red
 }
 
-# Agregar la recomendación de cambiar los permisos del archivo para evitar acceso no autorizado
 $archivo = Get-Item $nombre_archivo
 if ($archivo) {
     $acl = Get-Acl $nombre_archivo
