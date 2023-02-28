75db8b85597076bbeb88660a9df67d1f54f8b71e
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 19:00:38 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -1,4 +1,4 @@
-Verifica la versión de PowerShell y que se está ejecutando en un equipo con Windows
+#Verifica la versión de PowerShell y que se está ejecutando en un equipo con Windows
 if ($PSVersionTable.PSVersion.Major -lt 3) {
 Write-Host "Este script requiere PowerShell 3.0 o posterior." -ForegroundColor Red
 return
@@ -9,14 +9,14 @@ Write-Host "Este script solo se puede ejecutar en un equipo con Windows." -Foreg
 return
 }
 
-Cambiar el nombre del archivo de salida aquí:
+#Cambiar el nombre del archivo de salida aquí:
 $nombre_archivo = "pass_wifi.txt"
 
-Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
+#Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
 $contrasenas_wifi = netsh wlan show profile | Select-String "\bTodos los usuarios\b|\bUsuario actual\b" | %{(netsh wlan show profile name=$.matches[0].value key=clear)}
 $contrasenas_wifi = $contrasenas_wifi | Select-String "SSID name|Key Content" | %{($ -split ":\s+")[1]}
 
-Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
+#Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
 if (Test-Path -Path $nombre_archivo) {
 $confirm = Read-Host "El archivo $nombre_archivo ya existe. ¿Desea sobrescribirlo? (S/N)"
 if ($confirm -ne 'S') {
@@ -27,7 +27,7 @@ Remove-Item -Path $nombre_archivo -ErrorAction SilentlyContinue
 }
 }
 
-Escribir las contraseñas en el archivo de salida
+#Escribir las contraseñas en el archivo de salida
 try {
 $contrasenas_wifi | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
 Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
