56fd33cb862c51556c64784275b383d75899d0fc
Jhonnier Ardila <91538079+Cedaleon@users.noreply.github.com>
2023-02-27 19:36:44 -0500

    Update script_descarga_claves_wifi.ps1

--- script_descarga_claves_wifi.ps1
+++ script_descarga_claves_wifi.ps1
@@ -18,7 +18,7 @@ $contrasenas_wifi = foreach ($red in $redes_wifi) {
     $nombre_red = $red -replace ".*:\s*(.*)", '$1'
     $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear) -replace "(?ms).*Clave de seguridad.*:\s*(.*)\s*\n.*", '$1'
     if ($contrasena_red) {
-        "$nombre_red: $contrasena_red" | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
+        "$nombre_red: $contrasena_red"
     }
 }
 
@@ -35,6 +35,7 @@ if (Test-Path -Path $nombre_archivo) {
 
 # Escribir las contraseñas en el archivo de salida
 try {
+    $contrasenas_wifi | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
     Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
 } catch {
     Write-Host "Error al escribir en el archivo: $_.Exception.Message"
